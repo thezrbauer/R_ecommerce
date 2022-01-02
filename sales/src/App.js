741331260326bb37434cf4baec02a134c97de88a
Zack Bauer <zack.bauer30@gmail.com>
2022-01-02 16:43:27 -0500

    initializing

---
+++ sales/src/App.js
@@ -0,0 +1,26 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello</h1>
+      <header>
+        <h1>SNEAKERS</h1>
+          <div>
+            <p>Collectors</p>
+            <p>Men</p>
+            <p>Women</p>
+            <p>About</p>
+            <p>Contact</p>
+          </div>
+          <div>
+            <icon></icon>
+            <icon></icon>
+          </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
