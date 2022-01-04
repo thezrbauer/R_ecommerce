f62f2577f6d7a43c5e42e3b07082399d2fcce138
Zack Bauer <zack.bauer30@gmail.com>
2022-01-03 19:02:28 -0500

    html

--- sales/src/App.js
+++ sales/src/App.js
@@ -4,21 +4,48 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
       <header>
-        <h1>SNEAKERS</h1>
-          <div>
-            <p>Collectors</p>
-            <p>Men</p>
-            <p>Women</p>
-            <p>About</p>
-            <p>Contact</p>
+        <div>
+          <img>SNEAKers</img>
           </div>
-          <div>
-            <icon></icon>
-            <icon></icon>
+      <div className="wrapper">
+            <ul>
+            <li><a href="#">Collectors</a></li>
+            <li><a href="#">Men</a></li>
+            <li><a href="#">Women</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+            </ul>
+          </div>
+          <div className="icon_wrapper">
+            <icon>:D</icon>
+            <icon>D:</icon>
           </div>
       </header>
+      <div className="content">
+        <div className="left">
+            <div>
+              <div><img></img></div>
+            </div>
+            <div>
+              <div><img></img></div>
+              <div><img></img></div>
+              <div><img></img></div>
+              <div><img></img></div>
+            </div>
+
+        </div>
+
+        <div className="right">
+          <h3></h3>
+          <h2></h2>
+          <p></p>
+          <div>
+
+          </div>
+          
+        </div>
+      </div>
     </div>
   );
 }
