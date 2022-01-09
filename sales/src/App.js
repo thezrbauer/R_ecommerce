8defbbcd46a06ec23a759e60414d2d82f3d68e8e
Zack Bauer <zack.bauer30@gmail.com>
2022-01-09 16:22:13 -0500

    html/css

--- sales/src/App.js
+++ sales/src/App.js
@@ -7,26 +7,32 @@ import imageProduct2 from "./images/image-product-2-thumbnail.jpg"
 import imageProduct3 from "./images/image-product-3-thumbnail.jpg"
 import imageProduct4 from "./images/image-product-4-thumbnail.jpg"
 import avatar from "./images/image-avatar.png"
+import cartLogo from "./images/icon-cart.svg"
+import minusIcon from "./images/icon-minus.svg"
+import plusIcon from "./images/icon-plus.svg"
 
 function App() {
   return (
     <div className="App">
       <header>
-        <div className="assy">
-          <img className="img logo" src={logo} alt="logo"></img>
-          </div>
-      <div className="wrapper">
+        <div className="logo-wrapper">
+          <img className="logo" src={logo} alt="logo"></img>
+           </div>
+          <div className="wrapper">
             <ul>
             <li><a href="#">Collectors</a></li>
             <li><a href="#">Men</a></li>
             <li><a href="#">Women</a></li>
             <li><a href="#">About</a></li>
-            <li><a href="#">Contact</a>-</li>
+            <li><a href="#">Contact</a></li>
             </ul>
           </div>
-          <div className="icon_wrapper">
-          <button><img ></img></button>
-          <img src={avatar}></img>
+          <div className="smart-wrapper">
+
+          </div>
+          <div className="icon-wrapper">
+           <div className="cart"><img src={cartLogo} ></img></div>
+           <div className="avatar"><img src={avatar}></img></div>
           </div>
       </header>
       <div className="content">
@@ -44,11 +50,26 @@ function App() {
           <h3 className="company-name">SNEAKER COMPANY</h3>
           <h2 className="product-name">Fall Limited Sneakers</h2>
           <p className="product-description">These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer.</p>
-          <div>
+          
+          <div className="markdown-box">
+          <h3 className="markdown">$125.00</h3>
+          <p className="markdown-percent">50%</p>
+          </div>
+          <p className="original-price">$250.00</p>
+          
+          <div className="">
+            <div className="" ><span className="minus" data-src={minusIcon}></span></div>
+            <div className=""><span className=""></span></div>
+            <div className=""><span className=""></span></div>
+            <div className=""><span className=""></span></div>
+
 
           </div>
-          
+       
+       
         </div>
+
+
       </div>
     </div>
   );
